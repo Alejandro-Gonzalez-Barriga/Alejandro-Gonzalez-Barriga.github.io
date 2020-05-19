910f8a9e06d6611937ded4e0641615ad4ca304fe
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-05-19 17:09:30 -0500

    registered sw and arrange pwa icons

---
+++ js/app.js
@@ -0,0 +1,6 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then((reg) => console.log('sw registered', reg))
+    .catch((err) => console.log('sw not registered', err));
+}
---
+++ sw.js
@@ -0,0 +1,15 @@
+//install sw
+self.addEventListener('install', (evt) => {
+  console.log('sw has been installed');
+});
+
+//activate event
+self.addEventListener('activate', (evt) => {
+  console.log('sw has been activated');
+});
+
+//fetch event
+self.addEventListener('fetch', (evt) => {
+  //console.log('fetch event', evt);
+});
+//
