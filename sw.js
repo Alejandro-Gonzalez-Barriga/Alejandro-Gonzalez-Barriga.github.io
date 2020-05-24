9487486b014d6c18af2605b0701891e6f1eb6c36
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-05-23 21:35:35 -0500

    cached for off line use

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
+const staticCacheName = 'site-static';
+const assets = ['/', '/index.html', '/js/app.js'];
+
 //install sw
 self.addEventListener('install', (evt) => {
-  console.log('sw has been installed');
+  //console.log('sw has been installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('catching all assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 //activate event
@@ -11,5 +20,9 @@ self.addEventListener('activate', (evt) => {
 //fetch event
 self.addEventListener('fetch', (evt) => {
   //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
-//
